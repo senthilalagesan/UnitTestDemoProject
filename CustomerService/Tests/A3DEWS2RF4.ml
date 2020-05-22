d32f25b7ea1beb20a3fdc161296cb829b2681513
senthilalagesan <62317332+senthilalagesan@users.noreply.github.com>
2020-05-22 13:51:38 -0500

    Add files via upload

--- CustomerService/Tests/A3DEWS2RF4.ml
+++ CustomerService/Tests/A3DEWS2RF4.ml
@@ -1,9 +1,10 @@
 <?xml version="1.0" encoding="ASCII"?>
 <emulation:BWTFile xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:emulation="http:///emulation.ecore">
-  <CurrentLoadedTest href="platform:/resource/CustomerService/Tests/CustomerService1.bwt#/"/>
+  <CurrentLoadedTest href="platform:/resource/CustomerService/Tests/CustomerService3.bwt#/"/>
   <EmulationDataFile>ACED000574001B437573746F6D657253657276696365496D706C546573742E627774</EmulationDataFile>
   <EmulationDataFile>ACED0005740015436F756E74727948656C706572546573742E627774</EmulationDataFile>
   <EmulationDataFile>ACED0005740022437573746F6D657253657276696365496D706C5465737450726F636573732E627774</EmulationDataFile>
   <EmulationDataFile>ACED0005740014437573746F6D657253657276696365312E627774</EmulationDataFile>
   <EmulationDataFile>ACED0005740014437573746F6D657253657276696365322E627774</EmulationDataFile>
+  <EmulationDataFile>ACED0005740014437573746F6D657253657276696365332E627774</EmulationDataFile>
 </emulation:BWTFile>
